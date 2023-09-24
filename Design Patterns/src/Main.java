61223eb74f453ca6d9880026a6cbfb48e31836c4
sonek2706 <s26092@pjwstk.edu.pl>
2023-09-24 12:47:38 +0200

    Builder Implemented

--- Design Patterns/src/Main.java
+++ Design Patterns/src/Main.java
@@ -1,3 +1,5 @@
+import Singleton.Singleton;
+
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
--- Design Patterns/src/Singleton/Singleton.java
+++ Design Patterns/src/Singleton/Singleton.java
@@ -1,3 +1,5 @@
+package Singleton;
+
 public class Singleton {
 
     private static Singleton instance;

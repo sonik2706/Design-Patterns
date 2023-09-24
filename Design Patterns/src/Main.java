8129f2ff8dea43d0b6c3e8605de2bb740653d731
sonek2706 <s26092@pjwstk.edu.pl>
2023-09-24 10:32:06 +0200

    Implementation

--- Design Patterns/src/Main.java
+++ Design Patterns/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+
+        Singleton singleton = Singleton.getInstance();
+
+        System.out.println(singleton);
+
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton2);
     }
 }
